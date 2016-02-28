5d6cc67abbee61d9f3cecebd58e17d399e239e48
True <trueking7@gmail.com>
2016-02-28 12:12:40 -0500

    material design

--- app/src/main/java/ca/truewebdev/flickrbrowser/BaseActivity.java
+++ app/src/main/java/ca/truewebdev/flickrbrowser/BaseActivity.java
@@ -1,9 +1,22 @@
 package ca.truewebdev.flickrbrowser;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by True on 2/26/2016.
  */
 public class BaseActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    protected Toolbar activateToolbar() {
+        if(mToolbar == null) {
+            mToolbar = (Toolbar)
+                    findViewById(R.id.app_bar);
+            if(mToolbar != null) {
+                setSupportActionBar(mToolbar);
+            }
+        }
+        return mToolbar;
+    }
 }
+
--- app/src/main/java/ca/truewebdev/flickrbrowser/MainActivity.java
+++ app/src/main/java/ca/truewebdev/flickrbrowser/MainActivity.java
@@ -1,7 +1,6 @@
 package ca.truewebdev.flickrbrowser;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
@@ -10,7 +9,7 @@ import android.view.MenuItem;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     private static final String LOG_TAG = "MainActivity";
     private List<Photo> mPhotosList = new ArrayList<Photo>();
@@ -23,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
+
+        activateToolbar();
 
         mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -40,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
     }
+
+
 
 
     @Override
