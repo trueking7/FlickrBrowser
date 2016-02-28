b0eb529eb7e392833a95422dfb8201b600263fe2
True <trueking7@gmail.com>
2016-02-28 16:34:34 -0500

    editing material design

--- app/src/main/java/ca/truewebdev/flickrbrowser/BaseActivity.java
+++ app/src/main/java/ca/truewebdev/flickrbrowser/BaseActivity.java
@@ -7,7 +7,10 @@ import android.support.v7.widget.Toolbar;
  * Created by True on 2/26/2016.
  */
 public class BaseActivity extends AppCompatActivity {
+
     private Toolbar mToolbar;
+    public static final String FLICKR_QUERY = "FLICKR_QUERY";
+
     protected Toolbar activateToolbar() {
         if(mToolbar == null) {
             mToolbar = (Toolbar)
@@ -15,6 +18,14 @@ public class BaseActivity extends AppCompatActivity {
             if(mToolbar != null) {
                 setSupportActionBar(mToolbar);
             }
+        }
+        return mToolbar;
+    }
+
+    protected Toolbar activateToolbarWithHomeEnabled(){
+        activateToolbar();
+        if(mToolbar != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
         return mToolbar;
     }
